a293458746fd0d9107903ab3cc85ab8b174504c9
Jeff Andrews <88903188+cognizantJeff@users.noreply.github.com>
2022-03-27 17:09:24 -0400

    files updated for i/o lesson

--- src/com/semanticsquare/io/IOStreamsDemo.java
+++ src/com/semanticsquare/io/IOStreamsDemo.java
@@ -1,11 +1,8 @@
 package com.semanticsquare.io;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class IOStreamsDemo {
     static String inFileStr = "src/walden.jfif";
@@ -58,9 +55,130 @@ public class IOStreamsDemo {
         System.out.println("fileCopyWithBufferAndArray: " + (elapsedTime / 1000000.0) + " msec");
     }
 
+    private static void readFromStandardInput() {
+        System.out.println("\nInside readFromStandardInput ...");
+        String data;
+		/*
+		System.out.print("Enter \"start\" to continue (Using BufferedReader): ");
+
+		try (BufferedReader in = new BufferedReader(new InputStreamReader(System.in, "UTF-8"))){
+			while ((data = in.readLine()) != null && !data.equals("start")) {
+				System.out.print("\nDid not enter \"start\". Try again: ");
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println("Correct!!");
+		*/
+
+
+        System.out.print("\nEnter \"start\" to continue (Using java.util.Scanner): ");
+        Scanner scanner = new Scanner(System.in);
+
+        while(!(data = scanner.nextLine()).equals("start")) {
+            System.out.print("\nDid not enter \"start\". Try again: ");
+        }
+        System.out.println("Correct!!");
+
+
+        System.out.println("Now, enter the start code: ");
+        int code = 0; // other methods: nextLong, nextDouble, etc
+        try {
+            code = scanner.nextInt();
+        } catch (InputMismatchException e) {
+            e.printStackTrace();
+
+            System.out.println("you didn't enter a number");
+        } finally {
+            scanner.close();
+        }
+        System.out.println("Thanks. You entered code: " + code);
+
+
+        /**
+         * Scanner ~ a text scanner for parsing primitives & string
+         *         ~ breaks its input into tokens using a delimited pattern (default: whitespace)
+         *         ~ when System.in is used, internally constructor uses
+         *            an InputStreamReader to read from it
+         *         ~ hasXXX & nextXXX can be used together
+         *         ~ InputMismatchException is thrown
+         *         ~ From Java 5 onwards
+         */
+
+        Scanner s1 = new Scanner("Hello, How are you?");
+        while(s1.hasNext()) {
+            System.out.println(s1.next());
+        }
+    }
+
+    public static void fileMethodsDemo() {
+        System.out.println("\nInside fileMethodsDemo ...");
+
+        File f = new File("C:\\jid\\demo\\src\\..\\walden.jpg"); // "movies\\movies.txt" also works
+        //File f = new File("walden.jpg");
+
+        System.out.println("getAbsolutePath(): " + f.getAbsolutePath());
+        try {
+            System.out.println("getCanonicalPath(): " + f.getCanonicalPath());
+            System.out.println("createNewFile(): " + f.createNewFile());
+        } catch (IOException e) {}
+        System.out.println("separator: " + f.separator);
+        System.out.println("separatorChar: " + f.separatorChar);
+        System.out.println("getParent(): " + f.getParent());
+        System.out.println("lastModified(): " + f.lastModified());
+        System.out.println("exists(): " + f.exists());
+        System.out.println("isFile(): " + f.isFile());
+        System.out.println("isDirectory(): " + f.isDirectory());
+        System.out.println("length(): " + f.length());
+
+        System.out.println("My working or user directory: " + System.getProperty("user.dir"));
+        System.out.println("new File(\"testdir\").mkdir(): " + new File("testdir").mkdir());
+        System.out.println("new File(\"testdir\\test\").mkdir(): " + new File("testdir\\test").mkdir());
+        System.out.println("new File(\"testdir\").delete(): " + new File("testdir").delete());
+        System.out.println("new File(\"testdir\\test1\\test2\").mkdir(): " + new File("testdir\\test1\\test2").mkdir());
+        System.out.println("new File(\"testdir\\test1\\test2\").mkdirs(): " + new File("testdir\\test1\\test2").mkdirs());
+
+        try {
+            File f2 = new File("temp.txt");
+            System.out.println("f2.createNewFile(): " + f2.createNewFile());
+            System.out.println("f2.renameTo(...): " + f2.renameTo(new File("testdir\\temp1.txt"))); // move!!
+        } catch (IOException e) {}
+
+    }
+
+    public static void dirFilter(boolean applyFilter) {
+        System.out.println("\nInside dirFilter ...");
+
+        File path = new File(".");
+        String[] list;
+
+        if(!applyFilter)
+            list = path.list();
+        else
+            list = path.list(new DirFilter());
+
+        //Arrays.sort(list, String.CASE_INSENSITIVE_ORDER);
+        for(String dirItem : list)
+            System.out.println(dirItem);
+    }
+
     public static void main(String[] args) {
         //fileCopyNoBuffer();
-        fileCopyWithBufferAndArray();
-        System.out.println(System.getProperty("file.encoding"));
+//      fileCopyWithBufferAndArray();
+//      System.out.println(System.getProperty("file.encoding"));
+//        readFromStandardInput();
+        System.out.println(System.getProperty("user.dir"));//can always get root directory in this manner
+        fileMethodsDemo();
+        dirFilter(true);
     }
+
+
+}
+
+class DirFilter implements FilenameFilter {
+    // Holds filtering criteria
+    public boolean accept(File file, String name) {
+        return name.endsWith(".txt") || name.endsWith(".JPG");
+    }
+
 }
