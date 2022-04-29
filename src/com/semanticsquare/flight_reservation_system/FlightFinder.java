78df241175893d7a93509b5c327407b478a8af6f
Jeff Andrews <88903188+cognizantJeff@users.noreply.github.com>
2022-04-29 15:26:18 -0400

    adds extra example flights

--- src/com/semanticsquare/flight_reservation_system/FlightFinder.java
+++ src/com/semanticsquare/flight_reservation_system/FlightFinder.java
@@ -56,18 +56,62 @@ public class FlightFinder {
 
     public static void main(String[] args) {
         Flight f1 = new Flight(1, "1", "Emirates", "New York", "Dubai",
-                LocalDateTime.of(2017, 12, 20, 9, 0), LocalDateTime.of(2017, 12, 20, 9, 0));
+                LocalDateTime.of(2017, 12, 20, 9, 0),
+                LocalDateTime.of(2017, 12, 20, 9, 0));
         Flight f2 = new Flight(2, "2", "Delta", "San Francisco", "Paris",
-                LocalDateTime.of(2017, 12, 20, 9, 0), LocalDateTime.of(2017, 12, 20, 9, 0));
+                LocalDateTime.of(2017, 12, 20, 9, 0),
+                LocalDateTime.of(2017, 12, 20, 9, 0));
         Flight f3 = new Flight(3, "3", "Delta", "San Francisco", "Rome",
-                LocalDateTime.of(2017, 12, 20, 9, 0), LocalDateTime.of(2017, 12, 20, 9, 0));
+                LocalDateTime.of(2017, 12, 20, 9, 0),
+                LocalDateTime.of(2017, 12, 20, 9, 0));
         Flight f4 = new Flight(4, "4", "Emirates", "San Francisco", "Dubai",
-                LocalDateTime.of(2017, 12, 20, 8, 0), LocalDateTime.of(2017, 12, 20, 8, 0));
+                LocalDateTime.of(2017, 12, 20, 8, 0),
+                LocalDateTime.of(2017, 12, 20, 8, 0));
+
+
+        Flight f5 = new Flight(5, "5", "Emirates", "San Francisco", "Dubai",
+                LocalDateTime.of(2017, 12, 20, 9, 30),
+                LocalDateTime.of(2017, 12, 20, 9, 30));
+        Flight f6 = new Flight(6, "6", "Delta", "San Francisco", "Dubai",
+                LocalDateTime.of(2017, 12, 20, 11, 30),
+                LocalDateTime.of(2017, 12, 20, 11, 30));
+        Flight f7 = new Flight(7, "7", "Delta", "San Francisco", "Dubai",
+                LocalDateTime.of(2017, 12, 20, 12, 30),
+                LocalDateTime.of(2017, 12, 20, 12, 30));
+        Flight f8 = new Flight(8, "8", "Emirates", "San Francisco", "Dubai",
+                LocalDateTime.of(2017, 12, 20, 16, 0),
+                LocalDateTime.of(2017, 12, 20, 16, 0));
+
+
+        Flight f9 = new Flight(9, "9", "Emirates", "Paris", "New Delhi",
+                LocalDateTime.of(2017, 12, 20, 9, 0),
+                LocalDateTime.of(2017, 12, 20, 9, 0));
+        Flight f10 = new Flight(10, "10", "Delta", "Dubai", "Singapore",
+                LocalDateTime.of(2017, 12, 20, 9, 0),
+                LocalDateTime.of(2017, 12, 20, 9, 0));
+        Flight f11 = new Flight(11, "11", "Delta", "Dubai", "Mumbai",
+                LocalDateTime.of(2017, 12, 20, 9, 30),
+                LocalDateTime.of(2017, 12, 20, 9, 30));
+        Flight f12 = new Flight(12, "12", "Emirates", "Dubai", "Mumbai",
+                LocalDateTime.of(2017, 12, 20, 10, 30),
+                LocalDateTime.of(2017, 12, 20, 10, 30));
+        Flight f13 = new Flight(13, "13", "Emirates", "Dubai", "Mumbai",
+                LocalDateTime.of(2017, 12, 20, 12, 0),
+                LocalDateTime.of(2017, 12, 20, 12, 0));
+        Flight f14 = new Flight(14, "14", "Delta", "Dubai", "Mumbai",
+                LocalDateTime.of(2017, 12, 20, 14, 0),
+                LocalDateTime.of(2017, 12, 20, 14, 0));
+        Flight f15 = new Flight(15, "15", "Delta", "Rome", "Mumbai",
+                LocalDateTime.of(2017, 12, 20, 14, 0),
+                LocalDateTime.of(2017, 12, 20, 14, 0));
 
         Map<String, List<Flight>> allFlights = new HashMap<>();
 
         allFlights.put("New York", Arrays.asList(f1));
-        allFlights.put("San Francisco", Arrays.asList(f2, f3, f4));
+        allFlights.put("San Francisco", Arrays.asList(f2, f3, f4, f5, f6, f7, f8));
+        allFlights.put("Paris", Arrays.asList(f9));
+        allFlights.put("Dubai", Arrays.asList(f10, f11, f12, f13, f14));
+        allFlights.put("Rome", Arrays.asList(f15));
 
         List<NavigableSet<Flight>> result = new FlightFinder(allFlights).findFlights(20, 12, 2017, 9, 13, "San Francisco", "Dubai", "Mumbai", "America/Los_Angeles", "Asia/Dubai");
     }
